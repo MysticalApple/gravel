3c3352044c71290e034b27cfe14d2a3909305367
Ajay Gopinath <106454455+faattorney@users.noreply.github.com>
2023-03-26 20:52:33 -0700

    My real name is elton john

--- parsing.cu
+++ parsing.cu
@@ -1,13 +1,8 @@
 #include "parsing.h"
 
 /* Populate the vertices array with coords from the file */
-int parseVertices(FILE *object, VERTEX *vertices, const unsigned int vertexCount, int count) {
-    char fname[128];
-    printf("Which object file do you want to read?\n");
-    scanf("%127s", fname);
-    strcat(fname,".txt");
-    points = fopen(fname, "r");
-    vertex *vertices = (vertex *)malloc(count * sizeof(vertex));
+int parseVertices(FILE *object, VERTEX *vertices, const unsigned int vertexCount) {
+    VERTEX *vertices = (vertex *)malloc(vertexCount * sizeof(vertex));
     int c, prev_c = EOF;
     for (int i = 0; i < count; i++) {
         if (fscanf(points, "%lf %lf %lf", &vertices[i].x, &vertices[i].y, &vertices[i].z) != 3) {
@@ -20,21 +15,14 @@ int parseVertices(FILE *object, VERTEX *vertices, const unsigned int vertexCount
         prev_c = c;
     }
     return EOF;
-    fclose(points);
-    free(vertices);
 }
 
 /* Populate the edges array with edges from the file */
 int parseEdges(FILE *object, EDGE *edges, const unsigned int edgeCount, int count) {
-    char fname[128];
-    printf("Which object file do you want to read?\n");
-    scanf("%127s", fname);
-    strcat(fname,".txt");
-    points = fopen(fname, "r");
-    edge *wireframes = (edge *)malloc(count * sizeof(edge));
+    EDGE *edges = (edge *)malloc(edgeCount * sizeof(edge));
      int c, prev_c = EOF;
     for (int i = 0; i < count; i++) {
-        if (fscanf(points, "%hu %hu", &wireframes[i].punto1, &wireframes[i].punto2) != 2) {
+        if (fscanf(points, "%hu %hu", &wireframes[i].a, &wireframes[i].b) != 2) {
             return EOF;
         }
         c = getc(points);
