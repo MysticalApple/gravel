ad850e2f4afb8139ea052607f9c0b6ff79b4bf61
faattorney <funajay.play@gmail.com>
2023-03-26 18:27:52 -0700

    Updated so reads text files on input

--- points.c
+++ points.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct {
-    unsigned short g;
-} pointno;
+#include <string.h>
 
 typedef struct {
     double x;
@@ -16,17 +13,13 @@ typedef struct {
     unsigned short punto2;
 } edge;
 
-typedef struct {
-    unsigned short p;
-} edgeno;
-
-int parse1(FILE *points, pointno *nopoints, int count) {
+int parse1(FILE *points, unsigned short *nopoints, int count) {
     int c, prev_c = EOF;
     for (int i = 0; i < count; i++) {
-        if (fscanf(points, "%hu", &nopoints[i].g) != 1) {
+        if (fscanf(points, "%hu", &nopoints[i]) != 1) {
             return EOF;
         }
-        c = getchar();
+        c = getc(points);
         if (c == '\n' && prev_c == '\n') {
             return c;
         }
@@ -41,7 +34,7 @@ int parse2(FILE *points, vertex *vertices, int count) {
         if (fscanf(points, "%lf %lf %lf", &vertices[i].x, &vertices[i].y, &vertices[i].z) != 3) {
             return EOF;
         }
-        c = getchar();
+        c = getc(points);
         if(c == '\n' && prev_c == '\n') {
             return c;
         }
@@ -53,8 +46,10 @@ int parse2(FILE *points, vertex *vertices, int count) {
 int parse3(FILE *points, edge *wireframes, int count) {
     int c, prev_c = EOF;
     for (int i = 0; i < count; i++) {
-        fscanf(points, "%hu %hu", &wireframes[i].punto1, &wireframes[i].punto2);
-        c = getchar();
+        if (fscanf(points, "%hu %hu", &wireframes[i].punto1, &wireframes[i].punto2) != 2) {
+            return EOF;
+        }
+        c = getc(points);
         if(c == '\n' && prev_c == '\n') {
             return c;
         }
@@ -62,11 +57,14 @@ int parse3(FILE *points, edge *wireframes, int count) {
     }
     return EOF;
 }
-int parse4(FILE *points, edgeno *noedge, int count) {
+
+int parse4(FILE *points, unsigned short *noedge, int count) {
     int c, prev_c = EOF;
     for (int i = 0; i < count-1; i++) {
-        fscanf(points, "%hu", &noedge[i].p);
-        c = getchar();
+        if (fscanf(points, "%hu", &noedge[i]) != 1) {
+            return EOF;
+        }
+        c = getc(points);
         if(c == '\n' && prev_c == '\n') {
             return c;
         }
@@ -78,154 +76,40 @@ int parse4(FILE *points, edgeno *noedge, int count) {
 int main(void) {
     FILE *points;
     int count;
+    char fname[128];
+    printf("Which object file do you want to read?\n");
+    scanf("%127s", fname);
+    strcat(fname,".txt");
 
-    points = fopen("cube.txt", "r");
+    points = fopen(fname, "r");
     if (points == NULL) {
-        perror("cube.txt");
+        perror(fname);
         exit(1);
     }
     fscanf(points, "%d", &count);
-    pointno *nopoints = (pointno *)malloc(count *sizeof(pointno));
-    parse1(nopoints, nopoints, count);
+    unsigned short *nopoints = (unsigned short *)malloc(count * sizeof(unsigned short));
     vertex *vertices = (vertex *)malloc(count * sizeof(vertex));
-    parse2(points, vertices, count);
-    edge *wireframes = (edge *)malloc(count *sizeof(edge));
-    parse3(points, wireframes, count);
-    edgeno *noedge = (edgeno *)malloc(count *sizeof(edgeno));
-    parse4(points, noedge, count);
-    fclose(points);
-
-    for (int i = 0; i < count; i++) {
-        pointno n = nopoints[i];
-        printf("%hu\n", n.g);
-        vertex point = vertices[i];
-        printf("%lf %lf %lf\n", point.x, point.y, point.z);
-        edge line = wireframes[i];
-        printf("%hu %hu\n", line.punto1, line.punto2);
-        edgeno noedgeval = noedge[i];
-        printf("%hu\n", noedgeval.p);
-    }
-
-    free(nopoints);
-    free(vertices);
-    free(wireframes);
-    free(noedge);
-
-    points = fopen("Pentpyr.txt", "r");
-    if (points == NULL) {
-        perror("Pentpyr.txt");
-        exit(1);
-    }
-
-    fscanf(points, "%d", &count);
-    nopoints = (pointno *)malloc(count *sizeof(pointno));
+    edge *wireframes = (edge *)malloc(count * sizeof(edge));
+    unsigned short *noedgeval = (unsigned short *)malloc((count-1) * sizeof(unsigned short));
     parse1(points, nopoints, count);
-    vertices = (vertex *)malloc(count * sizeof(vertex));
     parse2(points, vertices, count);
-    wireframes = (edge *)malloc(count *sizeof(edge));
     parse3(points, wireframes, count);
-    noedge = (edgeno *)malloc(count *sizeof(edgeno));
-    parse4(points, noedge, count);
+    parse4(points, noedgeval, count);
     fclose(points);
 
     for (int i = 0; i < count; i++) {
-        pointno n = nopoints[i];
-        printf("%hu\n", n.g);
+        printf("%hu\n", nopoints[i]);
         vertex point = vertices[i];
         printf("%lf %lf %lf\n", point.x, point.y, point.z);
         edge line = wireframes[i];
         printf("%hu %hu\n", line.punto1, line.punto2);
-        edgeno noedgeval = noedge[i];
-        printf("%hu\n", noedgeval.p);
+        printf("%hu\n", noedgeval[i]);
     }
 
     free(nopoints);
     free(vertices);
     free(wireframes);
-    free(noedge);
-
-    points = fopen("tetrahedron.txt", "r");
-    if (points == NULL) {
-        perror("tetrahedron.txt");
-        exit(1);
-    }
-
-    fscanf(points, "%d", &count);
-    nopoints = (pointno *)malloc(count *sizeof(pointno));
-    parse1(points, nopoints, count);
-    vertices = (vertex *)malloc(count * sizeof(vertex));
-    parse2(points, vertices, count);
-    wireframes = (edge *)malloc(count *sizeof(edge));
-    parse3(points, wireframes, count);
-    noedge = (edgeno *)malloc(count *sizeof(edgeno));
-    parse4(points, noedge, count);
-    fclose(points);
-
-    for (int i = 0; i < count; i++) {
-        pointno n = nopoints[i];
-        printf("%hu\n", n.g);
-        vertex point = vertices[i];
-        printf("%lf %lf %lf\n", point.x, point.y, point.z);
-        edge line = wireframes[i];
-        printf("%hu %hu\n", line.punto1, line.punto2);
-        edgeno noedgeval = noedge[i];
-        printf("%hu\n", noedgeval.p);
-    }
-
-    free(nopoints);
-    free(vertices);
-    free(wireframes);
-    free(noedge);
-   
-
-
-    points = fopen("triprism.txt", "r");
-    if (points == NULL) {
-        perror("triprism.txt");
-        exit(1);
-    }
-
-    fscanf(points, "%d", &count);
-    nopoints = (pointno *)malloc(count *sizeof(pointno));
-    parse1(points, nopoints, count);
-    vertices = (vertex *)malloc(count * sizeof(vertex));
-    parse2(points, vertices, count);
-    wireframes = (edge *)malloc(count *sizeof(edge));
-    parse3(points, wireframes, count);
-    noedge = (edgeno *)malloc(count *sizeof(edgeno));
-    parse4(points, noedge, count);
-    fclose(points);
-
-    for (int i = 0; i < count; i++) {
-        pointno n = nopoints[i];
-        printf("%hu\n", n.g);
-        vertex point = vertices[i];
-        printf("%lf %lf %lf\n", point.x, point.y, point.z);
-        edge line = wireframes[i];
-        printf("%hu %hu\n", line.punto1, line.punto2);
-        edgeno noedgeval = noedge[i];
-        printf("%hu\n", noedgeval.p);
-    }
-
-    free(nopoints);
-    free(vertices);
-    free(wireframes);
-    free(noedge);
+    free(noedgeval);
    
     return 0;
 }
-
-/*int main(void) {
-    FILE *points = fopen("sphere.txt", "r");
-    if (points == NULL) {
-        perror("sphere.txt");
-        exit(1);
-    }
-    int count;
-    fscanf(points, "%d", &count);
-    vertex *vertices = (vertex *)malloc(count * sizeof(vertex));
-    parse(points, vertices, count);
-
-    fclose(points);
-*/
-    
